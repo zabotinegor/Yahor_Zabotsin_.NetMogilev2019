63ebb8af1190cbb2af6dfea2f0e8b6bb05172446
Егор Заботин <zabotinegor@mail.ru>
2019-10-08 18:27:55 +0300

    Module1 done

---
+++ StudySolution/Module1/Module1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            a -= b;
+            b += a;
+            a = b - a;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            Console.ReadKey();
+        }
+    }
+}
