cc9775c0cbd17a0d5635250e496dae0cbbe352dd
Егор Заботин <zabotinegor@mail.ru>
2019-10-09 18:57:25 +0300

    Task1 done

--- StudySolution/Module2/Task1/Task1/Program.cs
+++ StudySolution/Module2/Task1/Task1/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of companies: ");
+
+            if (int.TryParse(Console.ReadLine(), out var companyCount))
+            {
+                Console.WriteLine("Tax percentage (%): ");
+
+                if (double.TryParse(Console.ReadLine(), out var taxPercentage))
+                {
+                    Console.WriteLine($"Taxes from all companies will be {Tax.Calculate(companyCount, taxPercentage)}.");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong percentage!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong number!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ StudySolution/Module2/Task1/Task1/Tax.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1
+{
+    public static class Tax
+    {
+        const int Income = 500;
+
+        public static double Calculate(int companyCount, double taxPercentage)
+        {
+            return companyCount * Income * (taxPercentage / 100);
+        }
+    }
+}
