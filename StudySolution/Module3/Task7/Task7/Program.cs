49878d1391f26034542b4fef28d036c42ce2866d
Егор Заботин <zabotinegor@mail.ru>
2019-10-21 16:25:21 +0300

    Revert "Revert "Task7 ready""
    This reverts commit 29aac411a0c9dead32f9d87493380c301ba497dc.

--- StudySolution/Module3/Task7/Task7/Program.cs
+++ StudySolution/Module3/Task7/Task7/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace Task7
+﻿using System;
+using InputLib;
+
+namespace Task7
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (InputData.NatData("Enter size: ", out var size))
+            {
+                if (InputData.IntArray($"Enter {size} elemets: ", size, out var array))
+                {
+                    var previosElement = 0;
+
+                    Console.WriteLine("Elements: ");
+
+                    for (var i = 0; i < array.Length; i++)
+                    {
+                        
+                        if (i == 0)
+                        {
+                            previosElement = array[i];
+                            continue;
+                        }
+                        else if (previosElement < array[i])
+                        {
+                            Console.WriteLine($"{array[i]}");
+                        }
+                        previosElement = array[i];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid element!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid size!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
