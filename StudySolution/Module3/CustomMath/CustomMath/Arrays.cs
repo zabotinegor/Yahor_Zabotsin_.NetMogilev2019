b1dfd1ce6dd135a61e62cd8cb9859607d5a100f2
Егор Заботин <zabotinegor@mail.ru>
2019-10-21 18:01:54 +0300

    Task8 ready

--- StudySolution/Module3/CustomMath/CustomMath/Arrays.cs
+++ StudySolution/Module3/CustomMath/CustomMath/Arrays.cs
@@ -13,5 +13,43 @@
 
             return true;
         }
+
+        public static void FillHelix(ref int[,] array, int sizeY, int sizeX)
+        {
+            var sum = sizeX * sizeY;
+            var correctY = 0;
+            var correctX = 0;
+            var startNumber = 1;
+
+            while (sizeY > 0)
+            {
+                for (var side = 0; side < 4; side++)
+                {
+                    for (var x = 0; x < ((sizeX < sizeY) ? sizeY : sizeX); x++)
+                    {
+                        switch (side)
+                        {
+                            case 0 when x < sizeX - correctX && startNumber <= sum:
+                                array[side + correctY, x + correctX] = startNumber++;
+                                break;
+                            case 1 when x < sizeY - correctY && x != 0 && startNumber <= sum:
+                                array[x + correctY, sizeX - 1] = startNumber++;
+                                break;
+                            case 2 when x < sizeX - correctX && x != 0 && startNumber <= sum:
+                                array[sizeY - 1, sizeX - (x + 1)] = startNumber++;
+                                break;
+                            case 3 when x < sizeY - (correctY + 1) && x != 0 && startNumber <= sum:
+                                array[sizeY - (x + 1), correctY] = startNumber++;
+                                break;
+                        }
+                    }
+                }
+
+                sizeY--;
+                sizeX--;
+                correctY += 1;
+                correctX += 1;
+            }
+        }
     }
 }
--- StudySolution/Module3/Task8/Task8/Program.cs
+++ StudySolution/Module3/Task8/Task8/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomMath;
+using InputLib;
+using System;
 
 namespace Task8
 {
@@ -10,6 +8,30 @@ namespace Task8
     {
         static void Main(string[] args)
         {
+            if (InputData.NatData("Enter length: ", out var length) &&
+                InputData.NatData("Enter width: ", out var width))
+            {
+                var array = new int[length, width];
+
+                Arrays.FillHelix(ref array, array.GetLength(0), array.GetLength(1));
+                Console.WriteLine();
+
+                for (var i = 0; i < array.GetLength(0); i++)
+                {
+                    for (var j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write("{0}\t", array[i, j]);
+                    }
+
+                    Console.WriteLine("\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid parameters!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
