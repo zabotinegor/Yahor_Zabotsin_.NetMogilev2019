a700aeae441945c41cbe859ca36fa9ae30f548d2
Егор Заботин <zabotinegor@mail.ru>
2019-10-28 14:07:10 +0300

    added item A to task3

--- StudySolution/Module4/Task3/Task3/Program.cs
+++ StudySolution/Module4/Task3/Task3/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Task3
+﻿using System;
+using CustomMath;
+
+namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var random = new Random((int)DateTime.Now.Ticks);
+            var intA = random.Next(sbyte.MinValue, sbyte.MaxValue);
+            var intB = random.Next(sbyte.MinValue, sbyte.MaxValue);
+            var intC = random.Next(sbyte.MinValue, sbyte.MaxValue);
+            var doubleA = random.NextDouble();
+            var doubleB = random.NextDouble();
+            var doubleC = random.NextDouble();
+
+            Console.WriteLine($"Three integers before increase by 10: {intA}, {intB}, {intC}");
+
+            Integers.IncreaseBy10(ref intA, ref intB, ref intC);
+
+            Console.WriteLine($"Three integers after increase by 10: {intA}, {intB}, {intC}");
+
+
+            Console.ReadKey();
         }
     }
 }
