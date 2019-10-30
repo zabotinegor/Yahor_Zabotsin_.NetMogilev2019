6a11107a9c74d7379665fab72fb5d792dae059f2
Егор Заботин <zabotinegor@mail.ru>
2019-10-30 11:29:19 +0300

    Start task8

--- StudySolution/CustomMath/CustomMath/Roots.cs
+++ StudySolution/CustomMath/CustomMath/Roots.cs
@@ -4,33 +4,40 @@ namespace CustomMath
 {
     public static class Roots
     {
-        // решение уравнения F(x) = 0 на отрезке [a; b]
-        // с точностью eps>0 методом деления отрезка пополам
+        public delegate double FunctionDelegate(double x);
+        
         public static double Root(double a, double b, double eps, FunctionDelegate f)
         {
-            // корень гарантировано существует,
-            // если на концах отрезка
-            // функция принимает значения различных знаков.
             if (f(a) * f(b) > 0)
-                throw new Exception("Возможно, корней на этом отрезке не существует");
-            // обеспечиваем выполнение условия:
-            // a – левый конец отрезка, b – правый
+            {
+                throw new Exception("Perhaps the roots on this segment do not exist");
+            }
+            
+
             if (a > b)
             {
-                double t = a;
+                var t = a;
                 a = b;
                 b = t;
             }
-            // цикл метода деления отрезка пополам
-            while (b - a > eps)
+            
+            while ((b - a) > eps)
             {
-                double c = (a + b) / 2;
+                var c = (a + b) / 2;
+
                 if (f(c) == 0)
+                {
                     return c;
+                }
+
                 if (f(a) * f(c) < 0)
+                {
                     b = c;
+                }
                 else
+                {
                     a = c;
+                }
             }
             return (a + b) / 2;
         }
---
+++ StudySolution/Module4/Task8/Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
