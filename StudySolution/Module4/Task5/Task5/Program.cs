212b3107f850b09b7c2f7d09bbfe1c5ab9c133fc
Егор Заботин <zabotinegor@mail.ru>
2019-10-30 16:12:48 +0300

    Added some localization in task5

--- StudySolution/Module4/Task5/Task5/Program.cs
+++ StudySolution/Module4/Task5/Task5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using InputLib;
+using messages = Task5.Resources.Messages;
 
 namespace Task5
 {
@@ -10,13 +11,13 @@ namespace Task5
         {
             var dtfi = DateTimeFormatInfo.CurrentInfo;
 
-            Console.WriteLine("Enter the name of the month or its serial number:");
+            Console.WriteLine(messages.EnterMonth);
 
             var stringBuffer = Console.ReadLine();
 
-            if (InputData.IntData(stringBuffer, out var month) && (month >= 0) && (month < 12))
+            if (InputData.IntData(stringBuffer, out var month) && (month > 0) && (month <= 12))
             {
-                Console.WriteLine("{0}, {1} days", dtfi.MonthNames[month], DateTime.DaysInMonth(2019, month));
+                Console.WriteLine("{0}, {1}" + messages.StringDay, dtfi.MonthNames[month-1], DateTime.DaysInMonth(2019, month));
             }
 
             Console.ReadKey();
