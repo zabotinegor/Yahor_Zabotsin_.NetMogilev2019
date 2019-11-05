f9a80756a4594fb2eaf7a59f32f2790d9ca391f3
Егор Заботин <zabotinegor@mail.ru>
2019-11-05 17:25:00 +0300

    Start programm Game v2 without classes

---
+++ StudySolution/Module5/Gamev2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Gamev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var godMode = (args.Length != 0) && args[0].Equals("-g");
+
+
+        }
+    }
+}
