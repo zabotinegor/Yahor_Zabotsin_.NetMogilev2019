fb415fe4de4beeed69d821f62c53f00c55ba3419
Егор Заботин <zabotinegor@mail.ru>
2019-11-07 22:49:55 +0300

    Game done

--- StudySolution/Module5/Gamev2/Program.cs
+++ StudySolution/Module5/Gamev2/Program.cs
@@ -1,168 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using static Gamev2.Resources.Resources;
 using static System.Console;
 
 namespace Gamev2
 {
     class Program
     {
+        private const int BORDER_WIDTH = 10;
+        private const int BORDER_HEIGHT = 10;
+        private const int BOMB_COUNT = 10;
+        private const string GOD_MODE_KEY = "-g";
+
         static void Main(string[] args)
         {
-            #region Game parameters
-            bool godMode = (args.Length != 0) && args[0].Equals("-g");
-            int rightBorder = 10;
-            int downBorder = 10;
-            int bombCount = 10;
+            OutputEncoding = Encoding.UTF8;
+
+            bool godMode = (args.Length != 0) && args[0].Equals(GOD_MODE_KEY);
             IDictionary<(int x, int y), (bool active, int damage)> bombs;
-            Random random = new Random((int)DateTime.Now.Ticks);
-            #endregion
 
-            #region Symbols for display
-            const string symbolPerson = "@ ";
-            const string symbolCell = "X ";
-            const string symbolExit = "E";
-            const string symbolBomb = "o ";
-            const string symbolLife = "* ";
-            #endregion
-
-            #region User
-            WriteLine("Enter your name:");
+            WriteLine(SuggestInputUserName);
             var userName = ReadLine();
-            userName = string.IsNullOrEmpty(userName) ? "Player" : userName;
-            #endregion
+            userName = string.IsNullOrEmpty(userName) ? DefaultName : userName;
 
-            #region Main loop
             while (true)
             {
                 #region Display rules
+
                 Clear();
-                WriteLine("The player has 10 hit points.\n" +
-                          "10 traps are randomly set on the field, the damage of which is determined randomly (from 1 to 10).\n" +
-                          "Traps on the field are not visible, the player walks blindly. Does not go beyond the borders.\r\nGood luck.\n");
-                WriteLine($"{symbolPerson}- you\n" +
-                          $"{symbolExit}- exit");
+                WriteLine(godMode ? RulesForGodMode : Rules);
+                WriteLine(string.Concat(PersonMarker, YouTitle, "\n", ExitSymbol, ExitTitle));
+
                 var currentCursorLine = CursorTop;
+
                 #endregion
 
-                #region Session parameters
-                var lifePoints = 10;
+                #region Generate session parameters
+
+                int lifePoints = 10;
                 var personCoordinates = (x: 0, y: 0);
                 bombs = new Dictionary<(int x, int y), (bool active, int damage)>();
 
-                #region Generate bombs for session
-                for (var i = 0; i < bombCount; i++)
-                {
-                    (int x, int y) bombCoordinates;
-
-                    do
-                    {
-                        bombCoordinates = (random.Next(0, rightBorder - 1), random.Next(0, downBorder - 1));
-
-                    } while (bombs.ContainsKey(bombCoordinates) || bombCoordinates == (0, 0) ||
-                             bombCoordinates == (0, downBorder - 1) || bombCoordinates == (0, rightBorder - 1));
-
-                    bombs.Add(bombCoordinates, (true, random.Next(1, 10)));
-                }
-                #endregion
+                GenerateBombs(bombs);
 
                 #endregion
 
-                #region Game session loop
                 do
                 {
-                    #region Display user life
-                    var personLife = $"{userName}: ";
 
-                    for (var i = 0; i < lifePoints; i++)
-                    {
-                        personLife = string.Concat(personLife, symbolLife);
-                    }
-
-                    personLife = $"{personLife}\n";
                     SetCursorPosition(0, currentCursorLine);
-                    ClearCurrentConsoleLine();
-                    WriteLine(personLife);
-                    #endregion
 
-                    #region Display game field
-                    var field = string.Empty;
+                    DisplayUserLife(userName, lifePoints);
+                    DisplayGameField(personCoordinates, bombs, godMode);
 
-                    for (var i = 0; i < rightBorder; i++)
-                    {
-                        for (var j = 0; j < downBorder; j++)
-                        {
-                            var temp = ((j, i) == personCoordinates) ? symbolPerson :
-                                (bombs.TryGetValue((j, i), out var bomb1) && !bomb1.active) ? symbolBomb :
-                                (bombs.TryGetValue((j, i), out var bomb2) && godMode) ? $"{bomb2.damage} " : symbolCell;
+                    MovePerson(ref personCoordinates);
 
-                            field = string.Concat(field, ((i == rightBorder - 1) && (j == downBorder - 1)) ? symbolExit : temp);
-                        }
-
-                        field = $"{field}\n";
-                    }
-
-                    WriteLine(field);
-                    #endregion
-
-                    #region Move person
-                    switch (ReadKey().Key)
-                    {
-                        case ConsoleKey.UpArrow:
-                            personCoordinates.y -= (personCoordinates.y > 0) ? 1 : 0;
-                            break;
-                        case ConsoleKey.DownArrow:
-                            personCoordinates.y += (personCoordinates.y < (downBorder - 1)) ? 1 : 0;
-                            break;
-                        case ConsoleKey.LeftArrow:
-                            personCoordinates.x -= (personCoordinates.x > 0) ? 1 : 0;
-                            break;
-                        case ConsoleKey.RightArrow:
-                            personCoordinates.x += (personCoordinates.x < (rightBorder - 1)) ? 1 : 0;
-                            break;
-                        default:
-                            continue;
-                    }
-                    #endregion
-
-                    #region Person interaction with a bomb
-                    ClearCurrentConsoleLine();
-                    if (bombs.TryGetValue(personCoordinates, out var bomb) && bomb.active)
-                    {
-                        lifePoints -= godMode ? 0 : bomb.damage;
-                        ClearCurrentConsoleLine();
-                        WriteLine($"Boom!  -{bomb.damage}");
-                        bombs.Remove(personCoordinates);
-                        bombs.Add(personCoordinates, (active: false, damage: 0));
-                    }
-                    #endregion
+                    CheckBombExplosion(personCoordinates, bombs, ref lifePoints, godMode);
 
                     #region End Session
+
                     if (lifePoints <= 0)
                     {
-                        WriteLine("You lose!");
-                        ReadKey();
+                        WriteMessageAndWait(MessageYouLose);
                         break;
                     }
 
-                    if (personCoordinates == (downBorder - 1, rightBorder - 1))
+                    if (personCoordinates == (BORDER_HEIGHT - 1, BORDER_WIDTH - 1))
                     {
-                        WriteLine("You win!");
-                        ReadKey();
+                        WriteMessageAndWait(MessageYouWin);
                         break;
                     }
+
                     #endregion
 
                 } while (true);
-                #endregion
 
                 #region Request to continue or exit
+
                 Clear();
-                WriteLine("Would you like to continue? +/-");
+                WriteLine(MessageRequestToContinue);
                 ConsoleKey answerKey;
 
                 do
                 {
-                    ClearCurrentConsoleLine();
+                    ClearCurrentLine();
                     answerKey = ReadKey().Key;
 
                 } while ((answerKey != ConsoleKey.Add) && (answerKey != ConsoleKey.Subtract));
@@ -171,22 +93,122 @@ namespace Gamev2
                 {
                     break;
                 }
+
                 #endregion
             }
-            #endregion
 
             Clear();
-            WriteLine("Game over!");
-            ReadKey();
+            WriteMessageAndWait(MessageGameOver);
         }
 
-        private static void ClearCurrentConsoleLine()
+        private static void DisplayUserLife(string userName, int lifePoints)
+        {
+            var personLife = string.Concat(userName, ": ");
+
+            for (var i = 0; i < lifePoints; i++)
+            {
+                personLife = string.Concat(personLife, LifeSymbol);
+            }
+
+            RedrawCurrentLine(string.Concat(personLife, "\n"));
+        }
+
+        private static void CheckBombExplosion((int x, int y) personCoordinates,
+                                                IDictionary<(int x, int y), (bool active, int damage)> bombs,
+                                                ref int lifePoints,
+                                                bool godMode)
+        {
+            ClearCurrentLine();
+
+            if (bombs.TryGetValue(personCoordinates, out var bomb) && bomb.active)
+            {
+                lifePoints -= godMode ? 0 : bomb.damage;
+                RedrawCurrentLine($"{MessageExplosion}  -{bomb.damage}");
+                bombs.Remove(personCoordinates);
+                bombs.Add(personCoordinates, (active: false, damage: 0));
+            }
+        }
+
+        private static void DisplayGameField((int x, int y) personCoordinates,
+                                                IDictionary<(int x, int y), (bool active, int damage)> bombs,
+                                                bool godMode)
+        {
+            var field = string.Empty;
+
+            for (var i = 0; i < BORDER_WIDTH; i++)
+            {
+                for (var j = 0; j < BORDER_HEIGHT; j++)
+                {
+                    var temp = ((j, i) == personCoordinates) ? PersonMarker :
+                        (bombs.TryGetValue((j, i), out var bomb1) && !bomb1.active) ? BombSymbol :
+                        (bombs.TryGetValue((j, i), out var bomb2) && godMode) ? string.Concat(bomb2.damage, " ") : CellSymbol;
+
+                    field = string.Concat(field, ((i == BORDER_WIDTH - 1) && (j == BORDER_HEIGHT - 1)) ? ExitSymbol : temp);
+                }
+
+                field = string.Concat(field, "\n");
+            }
+
+            WriteLine(field);
+        }
+
+        private static void GenerateBombs(IDictionary<(int x, int y), (bool active, int damage)> bombs)
+        {
+            Random random = new Random((int)DateTime.Now.Ticks);
+
+            for (var i = 0; i < BOMB_COUNT; i++)
+            {
+                (int x, int y) bombCoordinates;
+
+                do
+                {
+                    bombCoordinates = (random.Next(0, BORDER_WIDTH - 1), random.Next(0, BORDER_HEIGHT - 1));
+
+                } while (bombs.ContainsKey(bombCoordinates) || bombCoordinates == (0, 0) ||
+                         bombCoordinates == (0, BORDER_HEIGHT - 1) || bombCoordinates == (0, BORDER_WIDTH - 1));
+
+                bombs.Add(bombCoordinates, (true, random.Next(1, 10)));
+            }
+        }
+
+        private static void MovePerson(ref (int x, int y) personCoordinates)
+        {
+            switch (ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow:
+                    personCoordinates.y -= (personCoordinates.y > 0) ? 1 : 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    personCoordinates.y += (personCoordinates.y < (BORDER_HEIGHT - 1)) ? 1 : 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    personCoordinates.x -= (personCoordinates.x > 0) ? 1 : 0;
+                    break;
+                case ConsoleKey.RightArrow:
+                    personCoordinates.x += (personCoordinates.x < (BORDER_WIDTH - 1)) ? 1 : 0;
+                    break;
+            }
+        }
+
+        private static void ClearCurrentLine()
         {
             var currentLineCursor = CursorTop;
 
             SetCursorPosition(0, CursorTop);
             Write(new string(' ', WindowWidth));
             SetCursorPosition(0, currentLineCursor);
+        }
+
+        private static void RedrawCurrentLine(string str)
+        {
+            ClearCurrentLine();
+            WriteLine(str);
+        }
+
+        private static void WriteMessageAndWait(string message)
+        {
+            WriteLine(message);
+            ReadKey();
         }
     }
 }
